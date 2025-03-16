f8362727dd09fb980c6b23137aba4822fea23bd5
Angelika Brown <angelikpbrown@gmail.com>
2025-03-16 15:18:44 -0400

    The loop is there

--- Hammurabi.py
+++ Hammurabi.py
@@ -10,24 +10,65 @@ class Hammurabi:
 
 
     def playGame(self):
-        self.population = 100
-        self.grain = 2800
-        self.land = 1000
-        self.land_val = 19
-        self.acresSold = 0
-        self.acresBought = 0
-        self.grainFood = 0
-        self.acresToPlant = 0
+        play_game = True
+        play_game = Hammurabi.start_game(play_game)
+
+
+        if play_game == False:
+            print("Leave.")
+        while play_game == True:
+
+
+            population = 100
+            grain = 2800
+            land = 1000
+            land_val = 19
+            acresSold = 0
+            sacresBought = 0
+            grainFood = 0
+            acresToPlant = 0
 
         # declare local variables here: grain, population, etc.
         # statements go after the declarations
+            for i in range(1,11):
 
-    # other methods go here
-    def askHowManyAcresToBuy(self,land_val):
+
+                print(
+                "=================================\n"
+                "O great Hammurabi!\n"
+                "You are in year " + str(i) + " of your ten year rule.\n"
+                "In the previous year 0 people starved to death.\n"
+                "In the previous year 5 people entered the kingdom.\n"
+                "The population is now " + str(population) + ".\n"
+                "We harvested " + str(grain) + " bushels at " + str(land_val) + " bushels per acre.\n"
+                "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n"
+                "The city owns " + str(land) + " acres of land.\n"
+                "Land is currently worth " + str(land_val) + " bushels per acre.\n"
+                "==================================\n"
+                )
+                if i == 10:
+                    return "end of loop"
+
+
+    #other methods go here
+
+    def start_game(x):
+        z = input("Our last ruler abandoned us after Plague #66. You're the only guy who said yes. Correct?\n")
+        if z == "No":
+            print("You can't back out now.")
+            x = False
+        if z == "Yes":
+            x = True
+        else:
+            print("Stop wasting our time.")
+            x = False
+        return x
+
+    def askHowManyAcresToBuy(self):
         acresSold = int(input("How many acres will you buy?"))
         return acresSold
 
-    def askHowManyAcresToSell(self,land_val):
+    def askHowManyAcresToSell(self):
         acresBought = int(input("How many acres will you sell?"))
         return acresBought
 
@@ -40,58 +81,37 @@ class Hammurabi:
         return acresToPlant
 
     def plagueDeaths(self, population):
-       if random.randint(0,99) < 16:
-        deaths = population / 2
+        if random.randint(0,99) < 16:
+            deaths = population / 2
         return deaths
 
     def starvationDeaths(self, population, bushelsFedToPeople):
         return 0
 
     def uprising(self, population, howManyPeopleStarved):
-       if howManyPeopleStarved > population * (45 / 100):
-        return True
+        if howManyPeopleStarved > population * (45 / 100):
+            return True
 
     def immigrants(self, population, acresOwned, grainInStorage):
-        pass
+            pass
 
     def harvest(self, acres, bushelsUsedAsSeed):
-        pass
+            pass
 
     def grainEatenByRats(self, bushels):
-        return 0
+            return 0
 
     def newCostOfLand(self):
         land_val = random.randint(17,23)
         return land_val
 
-    def summary(self, population, grain, land, land_val):
 
-
-        print(
-        "=================================\n"
-        "O great Hammurabi!\n"
-        "You are in year 1 of your ten year rule.\n"
-        "In the previous year 0 people starved to death.\n"
-        "In the previous year 5 people entered the kingdom.\n"
-        "The population is now " + str(population) + ".\n"
-        "We harvested " + str(grain) + " bushels at " + str(land_val) + " bushels per acre.\n"
-        "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n"
-        "The city owns " + str(land) + " acres of land.\n"
-        "Land is currently worth " + str(land_val) + " bushels per acre.\n"
-        "==================================\n"
-
-
-        )
 
     def finalSummary(self):
         pass
 
 
+
 if __name__ == "__main__":
     hammurabi = Hammurabi()
     hammurabi.main()
-    hammurabi.askHowManyAcresToBuy()
-    hammurabi.askHowManyAcresToSell()
-    hammurabi.askHowMuchGrainToFeedPeople()
-    hammurabi.askHowManyAcresToPlant()
-    hammurabi.summary(100,2800,1000,19)
